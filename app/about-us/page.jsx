dbb5cec84da75d858aaafc0fce70283c1d308330
Chambulila <kelvinchambulila5@gmail.com>
2025-01-14 17:07:34 +0300

    first commit

---
+++ app/about-us/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1 className='text-red-700'>About Us</h1>
+        <p>This is the about page.</p>
+        <a href="/">Go back to home</a>  
+    </div>
+  )
+}
