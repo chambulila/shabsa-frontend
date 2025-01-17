c58dcd9b0412adb56aa572cafd5067ffd03c057f
Chambulila <kelvinchambulila5@gmail.com>
2025-01-17 16:24:54 +0300

    checkout form

--- app/(home)/products/page.jsx
+++ app/(home)/products/page.jsx
@@ -1,34 +1,86 @@
+"use client"
 import Image from 'next/image';
 import { ShoppingCart } from 'lucide-react'; // Import shopping cart icon
 import { Button } from '@/components/ui/button';
 import { products } from '@/utils/localDb';
 
+import { useState } from 'react';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 const ProductCard = ({ product }) => {
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        phone: "",
+        address: "",
+        description: "",
+    });
+
+    const handleInputChange = (event, key) => {
+        setFormData({...formData, [key]: event.target.value });
+    };
+    async function create() {
+     console.log(formData)
+        // mutate data
+        // revalidate cache
+      }
     return (
         <div className="border rounded-lg shadow-sm hover:scale-105 hover:shadow-md transition duration-300" >
-            <div className="relative">
-              <Image
-                  src={product.image}
-                  alt={product.name}
-                  width={400} // Adjust as needed
-                  height={300}
-                  className="object-cover rounded-t-lg"
-              />
+            <Dialog open={open}>
+                <DialogContent className="w-full sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Delivery Details</DialogTitle>
+                        <DialogDescription>
+                            Please fill in your information below to complete your order.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <form action={create}>
+                    <div className="w-full space-y-4">
+                        <Input id="name" placeholder="Your Fullname" value={formData.name} onChange={(event) => handleInputChange(event, "name")} name="name" className="" />
+                        <Input id="phone" placeholder="Your Phone" value={formData.phone} onChange={(event) => handleInputChange(event, "phone")} name="phone" className="" />
+                        <Input id="address" placeholder="Address" value={formData.address} onChange={(event) => handleInputChange(event, "address")} name="address" className="" />
+                        <Textarea id="name" placeholder="Description here.." value={formData.description} onChange={(event) => handleInputChange(event, "description")} name="description" className="" />
+                    </div>
+                    <div className="flex gap-3 items-center mt-4">
+                      <Button onClick={() => setOpen(false)} className="w-full bg-secondary text-black border-2 hover:text-white hover:bg-gray-600 border-black">Cancel</Button>
+                      <Button type="submit" className="w-full hover:bg-blue-800">Submit</Button>
+                    </div>
+                    </form>
+                </DialogContent>
+            </Dialog>
+
+            <div className="relative cursor-pointer">
+                <Image
+                    src={product.image}
+                    alt={product.name}
+                    width={400} // Adjust as needed
+                    height={300}
+                    className="object-cover rounded-t-lg"
+                />
             </div>
 
             <div className="p-4">
-                <h3 className="text-lg font-semibold truncate">{product.name}</h3>
+                <h3 className="text-lg font-semibold truncate cursor-pointer ">{product.name}</h3>
                 <p className="text-sm text-gray-500">{`Tsh. ${product.price}`}</p>
                 <div className="flex items-center justify-between mt-2">
                     <div className="flex w-full justify-between gap-3">
-                    <Button className=" outline hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
-                        <ShoppingCart className="h-4 w-4 inline-block mr-1" />
-                        Add To Cart
-                    </Button>
-                    <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        <ShoppingCart className="h-4 w-4 inline-block mr-1" />
-                        Order Now
-                    </Button>
+                        <Button className=" outline hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+                            <ShoppingCart className="h-4 w-4 inline-block mr-1" />
+                            Add To Cart
+                        </Button>
+                        <Button onClick={() => setOpen(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <ShoppingCart className="h-4 w-4 inline-block mr-1" />
+                            Order Now
+                        </Button>
                     </div>
                 </div>
             </div>
