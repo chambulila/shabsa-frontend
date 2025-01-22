40b53224b1305773c9967443962dde01aac42957
Deo Gemini <geminideogratias8@gmail.com>
2025-01-22 22:42:23 +0300

    update export in next

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/',
-          permanent: false, // Set to true for a 301 redirect, or false for a 302 redirect
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
-  
+  output: 'export', // Enables static export
+
+  images: {
+    unoptimized: true, // Disables the image optimization API for static export
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: false, // Set to true for a 301 redirect, or false for a 302 redirect
+      },
+    ];
+  },
+};
+
+export default nextConfig;
