f84d5f71e532720d5e007e52c4f11820255cd856
Chambulila <kelvinchambulila5@gmail.com>
2025-01-26 00:43:33 +0300

    API_URL added

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: `${process.env.APP_URL}/api`,
+  baseURL: `${process.env.API_URL}/api`,
   //  'http://localhost:8000/api', // Laravel backend URL
   headers: {
     'Content-Type': 'application/json',
