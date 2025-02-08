9bea20b2d7f7c527757cc527ed49270517c90315
Chambulila <kelvinchambulila5@gmail.com>
2025-02-08 13:34:17 +0300

    allow http

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from "js-cookie";
 const api = axios.create({
-  baseURL: `https://shabsa.eportsolutions.co.tz/api`,
+  baseURL: `http://shabsa.eportsolutions.co.tz/api`,
   //  baseURL: 'http://localhost:8000/api', // Laravel backend URL
   //  baseURL: 'http://api.shabsa.co.tz/api', // Laravel backend URL
 //   headers: {
