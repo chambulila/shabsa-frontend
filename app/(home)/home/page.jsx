539d7a8bf0ebd0c549609f770726c4d468ad1da0
Chambulila <kelvinchambulila5@gmail.com>
2025-02-10 01:13:20 +0300

    optimize search engine with meta data

--- app/(home)/home/page.jsx
+++ app/(home)/home/page.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
 import Hero from "@/components/ui/user/hero";
+import Head from 'next/head';
 
 function page() {
   return (
-    <div><Hero />  </div>
+    <>
+      <Head>
+        <title>About Us - Shabsa Ceramics</title>
+        <meta name="description" content="Learn about Shabsa Ceramics & General Supply Ltd, your trusted source for building materials." />
+        <meta name="keywords" content="ceramics, building materials, tiles, marbles, Shabsa Ceramics" />
+        <meta name="author" content="Shabsa Ceramics" />
+        <meta property="og:title" content="About Us - Shabsa Ceramics" />
+        <meta property="og:description" content="Learn about Shabsa Ceramics & General Supply Ltd, your trusted source for building materials." />
+        <meta property="og:image" content="/logo1.png" />
+        <meta property="og:url" content="https://www.shabsa.com/about" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="About Us - Shabsa Ceramics" />
+        <meta name="twitter:description" content="Learn about Shabsa Ceramics & General Supply Ltd, your trusted source for building materials." />
+        {/* <meta name="twitter:image" content="/path/to/your/image.jpg" /> */}
+      </Head>
+      <Hero />
+    </>
+
+
   )
 }
 
