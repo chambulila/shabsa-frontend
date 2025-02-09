14c111e760ba294d2c1bdd3bad1ac77634d41c65
Chambulila <kelvinchambulila5@gmail.com>
2025-02-09 21:03:43 +0300

    image on about us page : live

--- app/dashboard/settings/about-us/page.jsx
+++ app/dashboard/settings/about-us/page.jsx
@@ -136,8 +136,8 @@ export default function page() {
                     {/* Left Image */}
                     <div className={`relative flex-shrink-0 w-full md:w-1/2 h-64 md:h-auto ${!fetchedAboutUs?.image ? 'bg-black' : ''}`}>
                         <Image
-                            src={fetchedAboutUs?.image || '/logo1.png'} // Replace with the actual image path
-                            alt="Crane"
+                            src={fetchedAboutUs?.image} // Replace with the actual image path
+                            alt="Image"
                             width={1000}
                             height={1000}
                             objectFit="cover"
