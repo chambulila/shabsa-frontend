0b9d3e7251fa12e7307e00a97886e2ffa5711f2f
Chambulila <kelvinchambulila5@gmail.com>
2025-02-10 01:28:33 +0300

    optimize search engine with meta data..

--- app/dashboard/settings/landing-page/page.jsx
+++ app/dashboard/settings/landing-page/page.jsx
@@ -1,8 +1,17 @@
 "use client"
 import LandingPageComponent from '@/components/landing-page/LandingPageComponent'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) {
+    return null;
+  }
+  
   return (
     <div>
         <LandingPageComponent />
